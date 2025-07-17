6a3fad885547e5d04eed69a4fb300fb9423322d0
Watkins <tmh.pug@gmail.com>
2025-07-18 00:07:00 +0700

    Update UI community activity chart

--- features/community/hooks/useCommunityTeamActivityAnalysis.tsx
+++ features/community/hooks/useCommunityTeamActivityAnalysis.tsx
@@ -1,10 +1,10 @@
 import { fetchCommunityTeamActivityAnalysis } from '@/apis';
 import { useQuery } from '@tanstack/react-query';
 
-export const useCommunityTeamActivityAnalysis = ({ communityId, fromDate, toDate }: { communityId: string, fromDate?: string, toDate?: string }) => {
+export const useCommunityTeamActivityAnalysis = ({ communityId, amount, unit }: { communityId: string, amount?: number, unit?: string }) => {
     return useQuery({
-        queryKey: ['communityTeamActivity', communityId, fromDate, toDate],
-        queryFn: () => fetchCommunityTeamActivityAnalysis({ communityId, fromDate, toDate }),
+        queryKey: ['communityTeamActivity', communityId, amount, unit],
+        queryFn: () => fetchCommunityTeamActivityAnalysis({ communityId, amount, unit }),
         enabled: !!communityId,
         staleTime: 1000 * 60 * 5,
     });
