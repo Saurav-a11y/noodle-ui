e1cb2ab0edb39efc204a89a07c8895b4fa3a06fb
Watkins <tmh.pug@gmail.com>
2025-08-04 11:13:32 +0700

    Update UI youtube live community

---
+++ components/common/loading/TwiiterCommunityLoading.tsx
@@ -0,0 +1,43 @@
+const TwitterCommunityLoading = () => {
+	return (
+		<div className="rounded-xl bg-white px-4 py-3 shadow-sm animate-pulse">
+			<div className="flex justify-between items-start">
+				<div className="flex items-center gap-3">
+					<div className="h-10 w-10 rounded-full bg-gray-200" />
+					<div className="flex flex-col gap-1">
+						<div className="h-3 w-28 bg-gray-200 rounded" />
+						<div className="h-3 w-20 bg-gray-100 rounded" />
+					</div>
+				</div>
+				<div className="h-6 w-24 rounded bg-gray-200" />
+			</div>
+
+			<div className="mt-3 h-4 w-3/4 bg-gray-200 rounded" />
+			<div className="mt-1 h-4 w-2/3 bg-gray-100 rounded" />
+
+			<div className="mt-3 flex items-center justify-between text-gray-400 text-xs">
+				<div className="flex items-center gap-5">
+					<div className="flex items-center gap-1">
+						<div className="h-3 w-3 rounded-full bg-gray-300" />
+						<div className="h-3 w-4 bg-gray-300 rounded" />
+					</div>
+					<div className="flex items-center gap-1">
+						<div className="h-3 w-3 rounded-full bg-gray-300" />
+						<div className="h-3 w-4 bg-gray-300 rounded" />
+					</div>
+					<div className="flex items-center gap-1">
+						<div className="h-3 w-3 rounded-full bg-gray-300" />
+						<div className="h-3 w-4 bg-gray-300 rounded" />
+					</div>
+					<div className="flex items-center gap-1">
+						<div className="h-3 w-3 rounded-full bg-gray-300" />
+						<div className="h-3 w-4 bg-gray-300 rounded" />
+					</div>
+				</div>
+				<div className="h-3 w-12 bg-gray-300 rounded" />
+			</div>
+		</div>
+	)
+}
+
+export default TwitterCommunityLoading;
--- features/cryptocurrency-detail/components/LiveActivity.tsx
+++ features/cryptocurrency-detail/components/LiveActivity.tsx
@@ -1,7 +1,7 @@
 import { Loader, MessageCircle, Bookmark, Eye } from "lucide-react";
 import XIcon from "@/icons/XIcon";
 import he from 'he'
-import { format } from 'date-fns';
+import { formatDistanceToNow } from 'date-fns';
 import GithubIcon from "@/icons/GithubIcon";
 import RedditIcon from "@/icons/RedditIcon";
 // import LightIcon from "@/icons/LightIcon";
@@ -21,6 +21,8 @@ import AuthenticIcon from "@/icons/AuthenticIcon";
 import UpVoteIcon from "@/icons/UpVoteIcon";
 import DownVoteIcon from "@/icons/DownVoteIcon";
 import RewardIcon from "@/icons/RewardIcon";
+import TwitterCommunityLoading from "@/components/common/loading/TwiiterCommunityLoading";
+import { YoutubeCommunityLoading } from "@/components/common/loading/YoutubeCommunityLoading";
 
 export const formatTweetText = (text: string): string => {
 	if (!text) return '';
@@ -64,6 +66,7 @@ const LiveActivity = () => {
 	const [activeTab, setActiveTab] = useState("Twitter");
 	const [page, setPage] = useState(1);
 	const [data, setData] = useState<any[]>([]);
+	console.log("🚀 ~ LiveActivity ~ data:", data)
 	const [isLoading, setIsLoading] = useState(false);
 	const [isLoadingMore, setIsLoadingMore] = useState(false);
 	const [totalItems, setTotalitems] = useState<number>(0);
@@ -77,8 +80,7 @@ const LiveActivity = () => {
 		});
 
 		const items = response?.data?.items || [];
-		console.log("🚀 ~ fetchData ~ items:", items)
-		const total = response?.data?.summary?.total_posts || 0;
+		const total = response?.data?.summary?.total_posts || response?.data?.summary?.total_videos || 0;
 
 		if (replace) {
 			setData(items);
@@ -189,15 +191,12 @@ const LiveActivity = () => {
 																			<span className="text-xs">@{isReTweet ? tweet?.info_retweet?.username : tweet?.username}</span>
 																		</div>
 																		<div className="flex items-center gap-2 text-[#4B4A4A] dark:text-white">
-																			<span className="text-xs opacity-50">{formatTimestamp(isReTweet ? tweet?.info_retweet?.created_at : tweet?.created_at)}</span>
+																			<span className="text-xs opacity-50">{formatDistanceToNow(new Date(isReTweet ? tweet?.info_retweet?.created_at : tweet?.created_at), { addSuffix: true, })}</span>
 																			<span>•</span>
 																			<span className="text-xs font-medium">{formatNumberShort(isReTweet ? tweet?.info_retweet?.followers_count : tweet?.followers_count ?? 0)} followers</span>
 																		</div>
 																	</div>
 																</div>
-																{/* {tweet?.verified && (
-																		<span className="bg-[#DDFFE4] text-[#16BC00] px-2 py-1 rounded-full text-xs font-reddit">Authentic</span>
-																	)} */}
 																<Link href={`https://x.com/${tweet?.username}/status/${tweet?.id}`} target="_blank" className="border border-[#E8E8E8] text-xs bg-white dark:bg-[#000] dark:hover:bg-[#222] px-2 py-1.5 rounded cursor-pointer font-reddit hidden md:block hover:bg-[#F0F0F0] transition-colors duration-200">View on Twitter</Link>
 															</div>
 														</div>
@@ -234,43 +233,7 @@ const LiveActivity = () => {
 											{!isLoadingMore && (
 												<div className="space-y-4">
 													{[...Array(3)].map((_, i) => (
-														<div key={i} className="rounded-xl bg-white px-4 py-3 shadow-sm animate-pulse">
-															<div className="flex justify-between items-start">
-																<div className="flex items-center gap-3">
-																	<div className="h-10 w-10 rounded-full bg-gray-200" />
-																	<div className="flex flex-col gap-1">
-																		<div className="h-3 w-28 bg-gray-200 rounded" />
-																		<div className="h-3 w-20 bg-gray-100 rounded" />
-																	</div>
-																</div>
-																<div className="h-6 w-24 rounded bg-gray-200" />
-															</div>
-
-															<div className="mt-3 h-4 w-3/4 bg-gray-200 rounded" />
-															<div className="mt-1 h-4 w-2/3 bg-gray-100 rounded" />
-
-															<div className="mt-3 flex items-center justify-between text-gray-400 text-xs">
-																<div className="flex items-center gap-5">
-																	<div className="flex items-center gap-1">
-																		<div className="h-3 w-3 rounded-full bg-gray-300" />
-																		<div className="h-3 w-4 bg-gray-300 rounded" />
-																	</div>
-																	<div className="flex items-center gap-1">
-																		<div className="h-3 w-3 rounded-full bg-gray-300" />
-																		<div className="h-3 w-4 bg-gray-300 rounded" />
-																	</div>
-																	<div className="flex items-center gap-1">
-																		<div className="h-3 w-3 rounded-full bg-gray-300" />
-																		<div className="h-3 w-4 bg-gray-300 rounded" />
-																	</div>
-																	<div className="flex items-center gap-1">
-																		<div className="h-3 w-3 rounded-full bg-gray-300" />
-																		<div className="h-3 w-4 bg-gray-300 rounded" />
-																	</div>
-																</div>
-																<div className="h-3 w-12 bg-gray-300 rounded" />
-															</div>
-														</div>
+														<TwitterCommunityLoading key={i} />
 													))}
 												</div>
 											)}
@@ -315,7 +278,7 @@ const LiveActivity = () => {
 																			<div className="mb-1">
 																				<span className="text-sm font-semibold">{activity.repo}</span>
 																				<div className="flex items-center gap-2 text-[#4B4A4A] dark:text-white">
-																					<span className="text-xs opacity-50">{format(new Date(activity?.created_at), 'dd/MM/yyyy')}</span>
+																					{/* <span className="text-xs opacity-50">{format(new Date(activity?.created_at), 'dd/MM/yyyy')}</span> */}
 																				</div>
 																			</div>
 																		</div>
@@ -465,78 +428,82 @@ const LiveActivity = () => {
 												<p className="font-semibold font-noto">YouTube Community Content</p>
 												<span className="ml-3 text-xs text-[#373737] dark:text-white font-reddit hidden md:block"><b>{formatNumberShort(totalItems)}</b> videos</span>
 											</div>
-											<button className="text-xs bg-white text-[#373737] dark:text-white dark:bg-[#000] dark:hover:bg-[#222] px-2 py-1.5 rounded font-reddit hidden md:block cursor-pointer hover:bg-[#F0F0F0] transition-colors duration-200">View Channel</button>
 										</div>
-										<div className="flex items-center justify-between mb-4 md:hidden">
-											<span className="ml-3 text-xs text-[#373737] font-reddit"><b>{formatNumberShort(totalItems)}</b> videos</span>
-											<button className="text-xs bg-white text-[#373737] dark:text-white dark:bg-[#000] dark:hover:bg-[#222] px-2 py-1.5 rounded font-reddit cursor-pointer hover:bg-[#F0F0F0] transition-colors duration-200">View Channel</button>
-										</div>
-										{_map(data, (video, i) => (
-											<div key={i} className="space-y-6">
-												{video?.recentVideos?.map((v, x) =>
-													<div key={x} className="bg-white dark:bg-[#000] dark:hover:bg-[#222] rounded-xl p-5">
-														<div className="">
-															<div className="flex items-start gap-3">
-																<div className="w-[100px] flex items-center justify-center text-white font-bold">
-																	<Image src="https://cdn.pixabay.com/photo/2016/11/19/03/08/youtube-1837872_1280.png" alt="Logo" width={320} height={320} className="w-[100px]" />
-																</div>
-																<div className="flex-1">
-																	<div className="mb-1 space-y-1">
-																		<span className="font-semibold font-noto text-[#373737] dark:text-white line-clamp-2">{v.title}</span>
-																		<div className="flex items-center gap-2 text-[#4B4A4A] dark:text-white font-noto hidden md:flex">
-																			<span className="text-xs opacity-50">{video.title}</span>
-																			<span>•</span>
-																			<span className="text-xs font-medium">{formatNumberShort(v.views)} views</span>
-																			<span>•</span>
-																			<span className="text-xs font-medium"> {format(new Date(v?.publishedAt), 'dd/MM/yyyy')}</span>
+										<div className="space-y-6">
+											{_map(data, (video, i) => (
+												<div key={i} className="bg-white dark:bg-[#000] dark:hover:bg-[#222] rounded-xl p-5">
+													<div className="">
+														<div className="flex items-start gap-3">
+															<div className="flex items-center justify-center text-white font-bold">
+																<svg height="50" width="100" version="1.1" id="Layer_1" xmlns="http:www.w3.org/2000/svg" xmlnsXlink="http:www.w3.org/1999/xlink"
+																	viewBox="0 0 461.001 461.001" xmlSpace="preserve">
+																	<g>
+																		<path style={{ fill: "#F61C0D" }} d="M365.257,67.393H95.744C42.866,67.393,0,110.259,0,163.137v134.728
+																	 	c0,52.878,42.866,95.744,95.744,95.744h269.513c52.878,0,95.744-42.866,95.744-95.744V163.137
+																	 	C461.001,110.259,418.135,67.393,365.257,67.393z M300.506,237.056l-126.06,60.123c-3.359,1.602-7.239-0.847-7.239-4.568V168.607
+																	 	c0-3.774,3.982-6.22,7.348-4.514l126.06,63.881C304.363,229.873,304.298,235.248,300.506,237.056z"/>
+																	</g>
+																</svg>
+															</div>
+															<div className="flex-1">
+																<div className="mb-1 space-y-1">
+																	<span className="font-semibold font-noto text-[#373737] dark:text-white line-clamp-2">{video?.title}</span>
+																	<div className="flex items-center gap-2 text-[#4B4A4A] dark:text-white font-noto hidden md:flex">
+																		<span className="text-xs opacity-50">{video?.channelTitle}</span>
+																		<span>•</span>
+																		<span className="text-xs font-medium">{formatNumberShort(video?.metrics?.views)} views</span>
+																		<span>•</span>
+																		<span className="text-xs font-medium">
+																			{video?.publishedAt
+																				? formatDistanceToNow(new Date(video.publishedAt), { addSuffix: true, })
+																				: 'Unknown'}
+																		</span>
+																	</div>
+																	<div className="flex items-center gap-4 text-xs font-noto text-[#4B4A4A] dark:text-white hidden md:flex">
+																		<div className="flex items-center gap-1">
+																			<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+																				<path d="M9.38733 3.97987C9.75898 3.561 10.2108 3.22087 10.7161 2.97956C11.2214 2.73824 11.77 2.60065 12.3294 2.5749C12.8888 2.54915 13.4476 2.63577 13.973 2.82964C14.4983 3.02351 14.9795 3.3207 15.388 3.70365C15.7966 4.0866 16.1243 4.54755 16.3517 5.05926C16.5791 5.57098 16.7017 6.12308 16.7121 6.68296C16.7226 7.24283 16.6207 7.79913 16.4126 8.31898C16.2044 8.83884 15.8942 9.31171 15.5003 9.70965L9.34169 15.9274C9.29686 15.9727 9.24349 16.0086 9.18468 16.0331C9.12587 16.0577 9.06277 16.0703 8.99905 16.0703C8.93532 16.0703 8.87223 16.0577 8.81342 16.0331C8.7546 16.0086 8.70124 15.9727 8.6564 15.9274L2.49783 9.70965C1.7511 8.95532 1.31837 7.94578 1.28699 6.88482C1.25561 5.82386 1.62792 4.79051 2.32876 3.99337C3.99248 2.10144 6.93805 2.09501 8.61076 3.97987L8.99905 4.41701L9.38733 3.97987Z" fill="#FF5959" />
+																			</svg>
+																			{video?.metrics?.likes}
 																		</div>
-																		<div className="flex items-center gap-4 text-xs font-noto text-[#4B4A4A] dark:text-white hidden md:flex">
-																			<div className="flex items-center gap-1">
-																				<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-																					<path d="M9.38733 3.97987C9.75898 3.561 10.2108 3.22087 10.7161 2.97956C11.2214 2.73824 11.77 2.60065 12.3294 2.5749C12.8888 2.54915 13.4476 2.63577 13.973 2.82964C14.4983 3.02351 14.9795 3.3207 15.388 3.70365C15.7966 4.0866 16.1243 4.54755 16.3517 5.05926C16.5791 5.57098 16.7017 6.12308 16.7121 6.68296C16.7226 7.24283 16.6207 7.79913 16.4126 8.31898C16.2044 8.83884 15.8942 9.31171 15.5003 9.70965L9.34169 15.9274C9.29686 15.9727 9.24349 16.0086 9.18468 16.0331C9.12587 16.0577 9.06277 16.0703 8.99905 16.0703C8.93532 16.0703 8.87223 16.0577 8.81342 16.0331C8.7546 16.0086 8.70124 15.9727 8.6564 15.9274L2.49783 9.70965C1.7511 8.95532 1.31837 7.94578 1.28699 6.88482C1.25561 5.82386 1.62792 4.79051 2.32876 3.99337C3.99248 2.10144 6.93805 2.09501 8.61076 3.97987L8.99905 4.41701L9.38733 3.97987Z" fill="#FF5959" />
-																				</svg>
-																				{v?.likes}
-																			</div>
-																			{/* <div className="flex items-center gap-1">
-																				<RotateIcon />
-																				{video.engagement.shares}
-																			</div> */}
-																			<div className="flex items-center gap-1">
-																				<ChatIcon />
-																				{v?.comments}
-																			</div>
+																		<div className="flex items-center gap-1">
+																			<ChatIcon />
+																			{video?.metrics?.comments}
 																		</div>
 																	</div>
 																</div>
 															</div>
-															<div className="flex items-center mt-2 md:mt-4 gap-2 text-[#4B4A4A] font-noto md:hidden">
-																<span className="text-xs opacity-50">{video.title}</span>
-																<span>•</span>
-																<span className="text-xs font-medium">{formatNumberShort(v.views)} views</span>
-																<span>•</span>
-																<span className="text-xs font-medium"> {format(new Date(v?.publishedAt), 'dd/MM/yyyy')}</span>
+														</div>
+														<div className="flex items-center mt-2 md:mt-4 gap-2 text-[#4B4A4A] font-noto md:hidden">
+															<span className="text-xs opacity-50">{video?.channelTitle}</span>
+															<span>•</span>
+															<span className="text-xs font-medium">{formatNumberShort(video?.metricsviews)} views</span>
+															<span>•</span>
+															<span className="text-xs font-medium">
+																{video?.publishedAt
+																	? formatDistanceToNow(new Date(video.publishedAt), { addSuffix: true, })
+																	: 'Unknown'}
+															</span>
+														</div>
+														<div className="flex items-center mt-2 md:mt-4 gap-4 text-xs font-noto text-[#4B4A4A] flex md:hidden">
+															<div className="flex items-center gap-1">
+																<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+																	<path d="M9.38733 3.97987C9.75898 3.561 10.2108 3.22087 10.7161 2.97956C11.2214 2.73824 11.77 2.60065 12.3294 2.5749C12.8888 2.54915 13.4476 2.63577 13.973 2.82964C14.4983 3.02351 14.9795 3.3207 15.388 3.70365C15.7966 4.0866 16.1243 4.54755 16.3517 5.05926C16.5791 5.57098 16.7017 6.12308 16.7121 6.68296C16.7226 7.24283 16.6207 7.79913 16.4126 8.31898C16.2044 8.83884 15.8942 9.31171 15.5003 9.70965L9.34169 15.9274C9.29686 15.9727 9.24349 16.0086 9.18468 16.0331C9.12587 16.0577 9.06277 16.0703 8.99905 16.0703C8.93532 16.0703 8.87223 16.0577 8.81342 16.0331C8.7546 16.0086 8.70124 15.9727 8.6564 15.9274L2.49783 9.70965C1.7511 8.95532 1.31837 7.94578 1.28699 6.88482C1.25561 5.82386 1.62792 4.79051 2.32876 3.99337C3.99248 2.10144 6.93805 2.09501 8.61076 3.97987L8.99905 4.41701L9.38733 3.97987Z" fill="#FF5959" />
+																</svg>
+																{video?.metrics?.likes}
 															</div>
-															<div className="flex items-center mt-2 md:mt-4 gap-4 text-xs font-noto text-[#4B4A4A] flex md:hidden">
-																<div className="flex items-center gap-1">
-																	<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-																		<path d="M9.38733 3.97987C9.75898 3.561 10.2108 3.22087 10.7161 2.97956C11.2214 2.73824 11.77 2.60065 12.3294 2.5749C12.8888 2.54915 13.4476 2.63577 13.973 2.82964C14.4983 3.02351 14.9795 3.3207 15.388 3.70365C15.7966 4.0866 16.1243 4.54755 16.3517 5.05926C16.5791 5.57098 16.7017 6.12308 16.7121 6.68296C16.7226 7.24283 16.6207 7.79913 16.4126 8.31898C16.2044 8.83884 15.8942 9.31171 15.5003 9.70965L9.34169 15.9274C9.29686 15.9727 9.24349 16.0086 9.18468 16.0331C9.12587 16.0577 9.06277 16.0703 8.99905 16.0703C8.93532 16.0703 8.87223 16.0577 8.81342 16.0331C8.7546 16.0086 8.70124 15.9727 8.6564 15.9274L2.49783 9.70965C1.7511 8.95532 1.31837 7.94578 1.28699 6.88482C1.25561 5.82386 1.62792 4.79051 2.32876 3.99337C3.99248 2.10144 6.93805 2.09501 8.61076 3.97987L8.99905 4.41701L9.38733 3.97987Z" fill="#FF5959" />
-																	</svg>
-																	{v?.likes}
-																</div>
-																{/* <div className="flex items-center gap-1">
-																	<RotateCcw className="w-4 h-4" />
-																	{video.engagement.shares}
-																</div> */}
-																<div className="flex items-center gap-1">
-																	<MessageCircle className="w-4 h-4" />
-																	{v?.comments}
-																</div>
+															<div className="flex items-center gap-1">
+																<MessageCircle className="w-4 h-4" />
+																{video?.metrics?.comments}
 															</div>
 														</div>
 													</div>
-												)}
-											</div>
-										))}
+												</div>
+											))}
+											{isLoadingMore && (
+												Array.from({ length: 5 }).map((_, i) => <YoutubeCommunityLoading key={i} />)
+											)}
+										</div>
 									</div>
 								)}
 							</>
