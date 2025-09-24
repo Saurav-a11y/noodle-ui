9acba68aad92f37b147bbb45a65f3b575fa8ae02
Watkins <tmh.pug@gmail.com>
2025-09-24 09:02:51 +0700

    Change text section total active users

--- features/stablecoins/TotalActiveUsersStablecoins.tsx
+++ features/stablecoins/TotalActiveUsersStablecoins.tsx
@@ -25,7 +25,7 @@ const TotalActiveUsersStablecoins = () => {
 	return (
 		<div className="p-4 bg-white dark:bg-black rounded-xl shadow-xl dark:text-white flex-1">
 			<div className="flex items-center gap-2 mb-2">
-				<h3 className="font-reddit">Number of Tracked Stablecoins</h3>
+				<h3 className="font-reddit">Total Active Users (7D)</h3>
 				<TooltipCommon content="The total number of crypto projects being monitored for community signals and on-chain metrics. Only projects with enough consistent data are included." />
 			</div>
 			{isGettingTotalActiveUserStableCoins ? (
