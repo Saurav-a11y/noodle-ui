0097e9c96efa4277fc0b6465b30bcb975af67f89
Watkins <tmh.pug@gmail.com>
2025-10-27 23:31:34 +0700

    Add docker file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,6 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
 	darkMode: "class",
 	content: [
-		'./src/**/*.{js,ts,jsx,tsx,mdx}',
 		'./app/**/*.{js,ts,jsx,tsx,mdx}',
 		'./components/**/*.{js,ts,jsx,tsx,mdx}',
 		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,15 +11,14 @@ const config: Config = {
 	],
 	theme: {
 		extend: {
-			colors: {
-				background: 'var(--background)',
-				foreground: 'var(--foreground)',
-			},
 			fontFamily: {
 				sans: ['var(--font-reddit)', 'sans-serif'],
 				reddit: ['var(--font-reddit)', 'sans-serif'],
 				noto: ['var(--font-noto)', 'sans-serif'],
 				space: ['var(--font-space)', 'sans-serif'],
+			},
+			experimental: {
+				optimizeCss: false,
 			},
 		},
 	},
